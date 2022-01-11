f55dd337c03e92ef31572386aed0ac6d792aff0e
okolobaha-me <yevheniy.onyshchenko@gmail.com>
2022-01-11 21:13:07 +0200

    added fetch func & render cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import './sass/main.scss';
+import fetchPhoto from './fetchPhoto';
+import renderGallery from './renderGallery';
+
+const refs = {
+  input: document.querySelector('.input'),
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const onclickSearchBtn = async e => {
+  e.preventDefault();
+
+  const response = await fetchPhoto(refs.input.value);
+  const photos = await response.json();
+
+  refs.gallery.innerHTML = renderGallery(photos.hits, counter);
+};
+
+refs.form.addEventListener('submit', onclickSearchBtn);
---
+++ src/renderGallery.js
@@ -0,0 +1,42 @@
+export default function renderPhotoList(photos) {
+  const photoListHtml = [];
+  for (const photo of photos) {
+    photoListHtml.push(renderPhotoCard(photo));
+  }
+
+  return photoListHtml.join('');
+}
+
+function renderPhotoCard({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+  return `
+  <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class='photo'/>
+  <div class="info">
+    <div class="wrapper">
+      <p class="info-item">
+        <b>Likes</b>
+      </p>
+      <p  class="info-item">${likes}</p>
+    </div>
+    <div class="wrapper">
+      <p class="info-item">
+        <b>Views</b>
+      </p>
+      <p  class="info-item">${views}</p>
+    </div>
+    <div class="wrapper">
+      <p class="info-item">
+        <b>Comments</b>
+      </p>
+      <p  class="info-item">${comments}</p>
+    </div>
+    <div class="wrapper">
+      <p class="info-item">
+        <b>Downloads</b>
+      </p>
+      <p  class="info-item">${downloads}</p>
+    </div>
+  </div>
+</div>
+  `;
+}
